75b31c84873e0b5028e7d53019a252415caad636
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-06-06 13:10:45 -0300

    siscopom - updates

--- app/resources/views/layouts/navigation.blade.php
+++ app/resources/views/layouts/navigation.blade.php
@@ -34,6 +34,18 @@
                     </x-slot>
 
                     <x-slot name="content">
+                    <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
