838b01ef0738100f3a62fce75c5274f183911fab
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-06-06 15:10:07 -0300

    siscopom - updates

--- app/resources/views/components/blocks.blade.php
+++ app/resources/views/components/blocks.blade.php
@@ -1,6 +1,6 @@
 <section class="px-4 mx-auto max-w-7xl">
     <div class="grid gap-6 lg:grid-cols-3 lg:gap-8">
-        <a href="https://laracasts.com" class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-gray-900 dark:ring-gray-800 dark:hover:text-white/70 dark:hover:ring-gray-700 dark:focus-visible:ring-[#FF2D20]">
+        <a href="https://laracasts.com" class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
             <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
                 <svg class="size-5 sm:size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <g fill="#FF2D20">
