de44c4fba9f6af6ab132b4118e287a4b7d7dfbde
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-06-06 15:20:38 -0300

    siscopom - updates

--- app/resources/views/reports/index.blade.php
+++ app/resources/views/reports/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-zinc-800 dark:text-zinc-200 leading-tight">
-            {{ __('Apps') }}
+            {{ __('Reports') }}
         </h2>
     </x-slot>
 
