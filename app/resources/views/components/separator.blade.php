e46773e68d6ce4367695bdfb09b367ff1d743aed
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-06-10 15:09:46 -0300

    siscopom - updates

---
+++ app/resources/views/components/separator.blade.php
@@ -0,0 +1,8 @@
+<div class="relative">
+    <div class="absolute inset-0 flex items-center" aria-hidden="true">
+        <div class="w-full border-t border-zinc-300 dark:border-zinc-500"></div>
+    </div>
+    <div class="relative flex justify-center">
+        <span class="px-2 text-sm text-zinc-800 dark:text-zinc-200 bg-zinc-100 dark:bg-zinc-800">Or continue with</span>
+    </div>
+</div>
