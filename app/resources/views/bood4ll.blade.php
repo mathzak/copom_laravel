5e62440e5eb512c9905b1b02e28b4c0858dee124
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-06-13 01:28:51 -0300

    copom - updates

--- app/resources/views/bood4ll.blade.php
+++ app/resources/views/bood4ll.blade.php
@@ -25,100 +25,109 @@
             @foreach ($components as $component)
             <div class="bg-white dark:bg-zinc-800 overflow-visible shadow-sm sm:rounded-lg mb-4">
                 <div class="p-6 text-zinc-900 dark:text-zinc-100">
-                    <section>
-                        <header>
-                            @if ($component['label'] ?? false ? true : false)
-                            <h2 class="text-lg font-medium text-zinc-900 dark:text-zinc-100">
-                                {{ __($component['label'] ?? null) }}
-                            </h2>
-                            @endif
-
-                            @if ($component['description'] ?? false ? true : false)
-                            <p class="mt-1 text-sm text-zinc-600 dark:text-zinc-400">
-                                {{ __($component['description'] ?? null) }}
-                            </p>
-                            @endif
-                        </header>
-
-                        @if ($component['type'] === 'form')
-                        <form method="post" action="{{ $component['action'] ?? null }}" class="mt-6 space-y-6">
-                            @csrf
-                            @method($component['method'] ?? 'get')
-
-                            @foreach ($component['fields'] ?? [] as $fields)
-                            <div class="flex w-full">
-                                @foreach ($fields as $field)
-
-                                @php
-                                $data = $component['data'] ?? [];
-                                @endphp
-
-                                <div class="{{ $field['class'] }} w-full">
-                                    <x-input-label for="{{ $field['name'] }}" :value="__($field['label'])" />
-                                    @if ($field['type'] == 'calendar')
-                                    <x-calendar id="{{ $field['name'] }}" name="{{ $field['name'] }}" type="text" class="mt-1 block w-full" :value="old($field['name'], $data[$field['name']] ?? null)" />
-                                    @elseif ($field['type'] == 'input')
-                                    <x-text-input id="{{ $field['name'] }}" name="{{ $field['name'] }}" type="text" mask="{{ $field['mask'] ?? null }}" class="mt-1 block w-full" :value="old($field['name'], $data[$field['name']] ?? null)" />
-                                    @elseif ($field['type'] == 'select')
-                                    <x-select id="{{ $field['name'] }}" name="{{ $field['name'] }}" :options="$field['options']" :multiple="$field['multiple'] ?? false" :value="old($field['name'], $data[$field['name']] ?? [])" />
-                                    @elseif ($field['type'] == 'toggle')
-                                    <x-toggle id="{{ $field['name'] }}" name="{{ $field['name'] }}" class="mt-1 block w-full" :checked="old($field['name'], $data[$field['name']] ?? false)" />
-                                    @endif
-                                    <x-input-error class="mt-2" :messages="$errors->get($field['name'])" />
-                                </div>
-                                @endforeach
-                            </div>
-                            @endforeach
-
-                            @if ($component['action'] ?? false ? true : false)
-                            <div class="flex items-center gap-4">
-                                <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-                                @if (session('status') === 'profile-updated')
-                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-zinc-600 dark:text-zinc-400">{{ __('Saved.') }}</p>
-                                @endif
-                            </div>
-                            @endif
-                        </form>
+                    <header>
+                        @if ($component['label'] ?? false)
+                        <h2 class="text-lg font-medium text-zinc-900 dark:text-zinc-100">
+                            {{ __($component['label'] ?? null) }}
+                        </h2>
                         @endif
 
-                        @if ($component['type'] === 'index')
-                        <script>
-                            window.appRoutes = {
-                                routeEdit: "{{ $edit ?? null ? route($edit, '__id__') : null }}"
-                            };
-                        </script>
+                        @if ($component['description'] ?? false)
+                        <p class="mt-1 text-sm text-zinc-600 dark:text-zinc-400">
+                            {{ __($component['description'] ?? null) }}
+                        </p>
+                        @endif
+                    </header>
+
+                    @if ($component['type'] === 'form')
+                    <form method="post" action="{{ $component['action'] ?? null }}" class="mt-6 space-y-6">
+                        @csrf
+                        @method($component['method'] ?? 'get')
+
+                        @foreach ($component['fields'] ?? [] as $fields)
 
                         @php
-                        $menu = $component['menu'] ?? [];
-                        $columns = $component['columns'] ?? [];
-                        $items = $component['data'] ?? [];
-                        $name = $component['nameColumn'] ?? [];
-                        $action = $component['action'] ?? null;
+                        $cols = collect($fields)->map(function ($item) {
+                        if (!isset($item['span'])) {
+                        $item['span'] = 1;
+                        }
+                        return $item;
+                        });
                         @endphp
 
-                        <div class="py-6">
-                            <x-table striped :menu="$menu ?? []" :columns="$columns ?? []" :rows="$items ?? []" table-text-link-label="Name">
-                                <x-slot name="tableTextLink">
-                                    <div>
-                                        @foreach ($name ?? [] as $item)
-                                        <template x-if="row.{{ $item['field'] }}">
-                                            <div x-text="row.{{ $item['field'] }}" class="{{ $item['class'] }}"></div>
-                                        </template>
-                                        @endforeach
-                                    </div>
-                                </x-slot>
-                                <x-slot name="tableActions">
-                                    <div x-data="{ rowId: row.id, editUrl: '' }" x-init="editUrl = `{{ route($action, '__id__') }}`.replace('__id__', rowId)">
-                                        <a :href="editUrl">
-                                            @svg('gmdi-edit-o', 'size-6', ['style' => 'color:#2D20FF'])
-                                        </a>
-                                    </div>
-                                </x-slot>
-                            </x-table>
+                        <div class="grid gap-4 grid-cols-{{ $cols->sum('span') }}">
+                            @foreach ($fields as $field)
+
+                            @php
+                            $field = collect($field)->toArray();
+                            $value = $component['data'] ?? [] ? $component['data'][$field['name']] : null;
+                            @endphp
+
+                            <div class="col-span-{{ $field['span'] ?? 1 }}">
+                                <x-input-label for="{{ $field['name'] }}" class="w-full" :value="__($field['label'])" />
+                                @if ($field['type'] == 'calendar')
+                                <x-calendar id="{{ $field['name'] }}" class="w-full" name="{{ $field['name'] }}" type="text" :value="old($field['name'], $value ?? null)" />
+                                @elseif ($field['type'] == 'input')
+                                <x-text-input id="{{ $field['name'] }}" class="w-full" name="{{ $field['name'] }}" type="text" mask="{{ $field['mask'] ?? null }}" :value="old($field['name'], $value ?? null)" />
+                                @elseif ($field['type'] == 'select')
+                                <x-select id="{{ $field['name'] }}" class="w-full" name="{{ $field['name'] }}" :options="$field['options']" :multiple="$field['multiple'] ?? false" :value="old($field['name'], $value ?? [])" />
+                                @elseif ($field['type'] == 'toggle')
+                                <x-toggle id="{{ $field['name'] }}" class="w-full" name="{{ $field['name'] }}" :checked="old($field['name'], $value ?? false)" />
+                                @endif
+                                <x-input-error class="mt-2" :messages="$errors->get($field['name'])" />
+                            </div>
+                            @endforeach
+                        </div>
+                        @endforeach
+
+                        @if ($component['action'] ?? false)
+                        <div class="flex items-center gap-4">
+                            <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+                            @if (session('status') === 'profile-updated')
+                            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-zinc-600 dark:text-zinc-400">{{ __('Saved.') }}</p>
+                            @endif
                         </div>
                         @endif
-                    </section>
+                    </form>
+                    @endif
+
+                    @if ($component['type'] === 'index')
+                    <script>
+                        window.appRoutes = {
+                            routeEdit: "{{ $edit ?? null ? route($edit, '__id__') : null }}"
+                        };
+                    </script>
+
+                    @php
+                    $menu = $component['menu'] ?? [];
+                    $columns = $component['columns'] ?? [];
+                    $items = $component['data'] ?? [];
+                    $name = $component['nameColumn'] ?? [];
+                    $action = $component['action'] ?? null;
+                    @endphp
+
+                    <div class="py-6">
+                        <x-table striped :menu="$menu" :columns="$columns" :rows="$items" table-text-link-label="Name">
+                            <x-slot name="tableTextLink">
+                                <div>
+                                    @foreach ($name ?? [] as $item)
+                                    <template x-if="row.{{ $item['field'] }}">
+                                        <div x-text="row.{{ $item['field'] }}" class="{{ $item['class'] }}"></div>
+                                    </template>
+                                    @endforeach
+                                </div>
+                            </x-slot>
+                            <x-slot name="tableActions">
+                                <div x-data="{ rowId: row.id, editUrl: '' }" x-init="editUrl = `{{ route($action, '__id__') }}`.replace('__id__', rowId)">
+                                    <a :href="editUrl">
+                                        @svg('gmdi-edit-o', 'size-6', ['style' => 'color:#2D20FF'])
+                                    </a>
+                                </div>
+                            </x-slot>
+                        </x-table>
+                    </div>
+                    @endif
                 </div>
             </div>
             @endforeach
--- app/resources/views/components/toggle.blade.php
+++ app/resources/views/components/toggle.blade.php
@@ -1,9 +1,9 @@
 @props(['id', 'name', 'checked' => false])
 
 <div x-data="{ checked: {{ $checked ? 'true' : 'false' }} }">
-    <button type="button" @click="checked = !checked; $refs.hiddenInput.value = checked ? 1 : 0" :class="checked ? 'bg-green-500' : 'bg-red-500'" class="text-white font-bold mt-1 rounded w-full h-10 flex items-center justify-center">
+    <button type="button" @click="checked = !checked" :class="checked ? 'bg-green-500' : 'bg-red-500'" class="text-white font-bold rounded w-full h-10 flex items-center justify-center">
         <x-gmdi-check-o x-show="checked" class="size-6" />
         <x-gmdi-clear-o x-show="!checked" class="size-6" />
     </button>
-    <input type="hidden" id="{{ $id }}" name="{{ $name }}" x-ref="hiddenInput" :value="checked ? 1 : 0">
+    <input type="hidden" id="{{ $id }}" name="{{ $name }}" x-model="checked" />
 </div>
